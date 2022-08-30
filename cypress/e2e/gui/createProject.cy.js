87a852b30687aca5c3d1a438611973f2577f8f87
Eduardo Manoel <eduardomanoelnn@yahoo.com.br>
2022-08-29 22:51:20 -0300

    main

--- cypress/e2e/gui/createProject.cy.js
+++ cypress/e2e/gui/createProject.cy.js
@@ -1,19 +1,18 @@
 /// <reference types="Cypress" />
 
 
-const cypress = require('cypress')
 const faker = require('faker')
 describe('Create Project', () => {
     beforeEach (() => cy.login ())
 
-    it.only('successfully', ()=>{
+    it('successfully', ()=>{
         const project = {
             name: `project-${faker.random.uuid()}`,
             description: faker.random.words(10)
         }
         cy.gui_createProject(project)
         cy.url()
-        .should ('be.equal',`${cypress.defineConfig('baseUrl')}${Cypress.env('user_name')}/${project.name}`)
+        .should ('be.equal', `${Cypress.config('baseUrl')}${(Cypress.env('user_name'))}/${project.name}`)
         cy.contains(project.name)
         .should('be.visible')
         cy.contains(project.description)
